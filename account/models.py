fc05df059a799083388114b155688e243481ad71
Amilkar Munoz <yellowballpython@gmail.com>
2021-03-07 20:27:42 -0600

    Account github field added

--- account/models.py
+++ account/models.py
@@ -3,10 +3,14 @@ from languages.models import Language, Framework
 from users.models import UserModel
 
 class Account(models.Model):
+    # GENERAL
     user = models.OneToOneField(UserModel, on_delete=models.CASCADE, related_name='account')
     dob = models.DateField(verbose_name='Fecha de nacimiento', blank=True, null=True)
-    language = models.ManyToManyField(Language, blank=True, related_name="account")
-    framework = models.ManyToManyField(Framework, blank=True, related_name="account")
+    languages = models.ManyToManyField(Language, blank=True, related_name="account")
+    frameworks = models.ManyToManyField(Framework, blank=True, related_name="account")
+    # SOCIAL
+    github = models.URLField(verbose_name='github', blank=True, null=True)
+    # ACCOUNT STATUS
     is_active = models.BooleanField(default=True)
     is_tutor = models.BooleanField(default=False)
 
